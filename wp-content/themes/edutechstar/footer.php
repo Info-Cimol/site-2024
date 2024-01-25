1c5f72508a81d467bb3a84d39ac6c52313c7aa92
Ketlein <ketleindasilva2020@@gmail.com>
2024-01-25 16:16:45 -0300

    24

--- wp-content/themes/edutechstar/footer.php
+++ wp-content/themes/edutechstar/footer.php
@@ -17,30 +17,18 @@
         <div class="mu-footer-top-area">
           <div class="row">
           <?php if( is_active_sidebar( 'footer-widget-area-1') ) { ?>
-            <div class="col-lg-3 col-md-3 col-sm-3">
+            <div class="col-lg-6 col-md-6 col-sm-6">
               <div class="mu-footer-widget">
               <?php dynamic_sidebar( 'footer-widget-area-1' ); ?>
               </div>
             </div>
-            <?php } if( is_active_sidebar( 'footer-widget-area-2') ) { ?>
-            <div class="col-lg-3 col-md-3 col-sm-3">
+            <?php } if( is_active_sidebar( 'footer-widget-area-2') )?>
+            <div class="col-lg-6 col-md-6 col-sm-6">
               <div class="mu-footer-widget">
               <?php dynamic_sidebar( 'footer-widget-area-2' ); ?>
               </div>
             </div>
-            <?php } if( is_active_sidebar( 'footer-widget-area-3') ) { ?>
-            <div class="col-lg-3 col-md-3 col-sm-3">
-              <div class="mu-footer-widget">
-              <?php dynamic_sidebar( 'footer-widget-area-3' ); ?>
-              </div>
-            </div>
-            <?php } if( is_active_sidebar( 'footer-widget-area-4') ) { ?>
-            <div class="col-lg-3 col-md-3 col-sm-3">
-              <div class="mu-footer-widget">
-              <?php dynamic_sidebar( 'footer-widget-area-4' ); ?>
-              </div>
-            </div>
-            <?php } ?>
+      
           </div>
         </div>
       </div>
--- wp-content/themes/edutechstar/index.php
+++ wp-content/themes/edutechstar/index.php
@@ -34,7 +34,7 @@ get_header(); ?>
       <div class="col-md-12">
         <div class="mu-course-content-area">
           <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-8">
               <!-- start course content container -->
               <div class="mu-course-container mu-blog-archive">
                 <div class="row">
@@ -48,18 +48,9 @@ get_header(); ?>
                     get_template_part('template-parts/content', 'none');
                   } ?>
                 </div>
-                 <nav>
-                   <ul class="pagination comments-pagination">
-                     <?php 
-                          the_posts_pagination( array(
-                          'prev_text'          => '<i class="fa fa-angle-double-left"></i>',
-                          'next_text'          => '<i class="fa fa-angle-double-right"></i>',
-                          ) ); ?>
-                     </ul>
-                  </nav>
               </div>
              </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
               <aside class="mu-sidebar">
                 <div class="mu-single-sidebar">
                   <?php get_sidebar(); ?>
